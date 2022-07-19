28fc17b1852c1872cf3f1c19b618cf7da322e142
divyasn21 <103387570+divyasn21@users.noreply.github.com>
2022-07-19 21:24:21 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
-print("Hello World Demo...19 Jul 2022 git webhook demo \n")
+print("Hello World Demo...20 Jul 2022 git webhook demo \n")
 
 
 
